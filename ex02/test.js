49f642ed426fdba106b7b07420f51e44127fae5d
woodstock1993 <diqkqkqk@gmail.com>
2021-04-05 15:57:13 +0900

    Vanila_JS

--- ex02/test.js
+++ ex02/test.js
@@ -1,25 +1,19 @@
-const clockContainer = document.querySelector(".js-clock"),
-	clockTitle = clockContainer.querySelector("h1");
-
-const SECONDS = 1000;
-const MINUTES = SECONDS * 60;
-const HOURS = MINUTES * 60;
-const DAYS = HOURS * 24;
-
-function countDown() {
-	  const xmasDay = new Date("2020-12-24:00:00:00+0900").getTime();
-	  const currentDay = new Date().getTime();
-	  const remainDay = xmasDay - currentDay;
-	  const days = Math.floor(remainDay / DAYS);
-	  const hours = Math.floor((remainDay % DAYS) / HOURS);
-	  const minutes = Math.floor((remainDay % HOURS) / MINUTES);
-	  const seconds = Math.floor((remainDay % MINUTES) / SECONDS);
-
-  clockTitle.innerText = `${days < 10 ? `0${days}` : days + "d"} ${
-	  hours < 10 ? `0${hours}` : hours + "h"
-  } ${minutes < 10 ? `0${minutes}` : minutes + "m"} ${
-	  seconds < 10 ? `0${seconds}` : seconds + "s"
-  }`;
+const select = document.querySelector("select");
+const COUNTRY_NAME = "Country";
+function saveCounrty() {
+	  const changeValue = select.value;
+	  localStorage.setItem(COUNTRY_NAME, changeValue);
 }
 
-setInterval(countDown, 1000);
+function loadCountries(){
+	  const selected = localStorage.getItem("COUNTRY_NAME");
+	  if (selected) {
+		const option = document.querySelector(`option[value="${selected}]")`);
+		option.selected = true; 
+  }
+}
+
+function init() {
+	  select.addEventListener("change", saveCounrty);
+}
+init();
